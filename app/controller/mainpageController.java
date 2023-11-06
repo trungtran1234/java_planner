f7ca63c16bc3748aa5007e614175f7f8f780fad6
food0903 <phuclthg@gmail.com>
2023-11-05 17:45:14 -0800

    a few changes

--- app/controller/mainpageController.java
+++ app/controller/mainpageController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
+
 
 import java.awt.*;
 import java.net.URL;
@@ -31,12 +33,16 @@ public class mainpageController implements Initializable{
     @FXML
     private ChoiceBox<String> dayOfWeek;
 
+    @FXML
+    private Button enterScheduleButton;
+
     private String[] options = {"1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00"};
-    private boolean isAm = true;
 
     private String[] amOrPm = {"AM", "PM"};
 
     private String[] weekDay = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -47,7 +53,8 @@ public class mainpageController implements Initializable{
         dayOfWeek.getItems().addAll(weekDay);
     }
 
-    public void submitButtonAction(ActionEvent event) {
+
+    public void enterScheduleButtonAction(ActionEvent event) {
         // Get the user input fields
         String startTime = startTimeSelector.getValue();
         String endTime = endTimeSelector.getValue();
@@ -58,13 +65,33 @@ public class mainpageController implements Initializable{
         String startTimeResult = startTime + " " + startTimeAmOrPm;
         String endTimeResult = endTime + " " + endTimeAmOrPm;
 
+
+
+
+        updateEventStatus(day, startTimeResult);
+    }
+
+    public void updateEventStatus(String day, String startTimeResult) {
         dbConnection connectNow = new dbConnection();
         Connection connection = connectNow.connect();
+        // Assuming you have a table named 'schedule' in your database
+        String sql = "UPDATE schedule SET is_event_scheduled = 1 WHERE day_of_week = ? AND time_slot = ?";
 
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, day);
+            statement.setString(2, startTimeResult);
+            statement.executeUpdate();
+            System.out.println("changed");
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("SQL Error: " + e.getMessage());
 
-
+            // Handle the exception
+        }
     }
+
+
 
 
 }
